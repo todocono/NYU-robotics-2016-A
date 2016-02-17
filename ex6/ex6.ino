164dc2da373a0e1abd8bb60193e5ea0bf60f0a4e
Rodolfo Cossovich <todocono@gmail.com>
2016-02-17 15:49:29 +0800

    improved example of ultrasound  SR04

--- ex6/ex6.ino
+++ ex6/ex6.ino
@@ -1,63 +1,94 @@
-#include <Servo.h> 
+#include <Servo.h>
 
-unsigned long actualDistance = 0;
- 
-Servo myservo;  // create servo object to control a servo 
-int pos = 60; 
-int sweepFlag = 1;
- 
- 
-int URPWM = 3; // PWM Output 0－25000US，Every 50US represent 1cm
-int URTRIG= 10; // PWM trigger pin
-uint8_t EnPwmCmd[4]={0x44,0x02,0xbb,0x01};    // distance measure command
-  
-void setup(){                                 // Serial initialization
-  myservo.attach(9); 
-  Serial.begin(9600);                         // Sets the baud rate to 9600
-  SensorSetup();
+/*
+  Example g
+   Introduction to Robotics
+   by Rodolfo Cossovich at New York University
+   Description: This software uses HC-SR04 on pins 13 and 12
+
+   Original code improvements to the Ping sketch sourced from Trollmaker.com . More info at: http://goo.gl/kJ8Gl
+   Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+
+Servo myservo1;
+Servo myservo2;
+
+#define trigPin 5
+#define echoPin 6
+
+int speedPin_M1 = 5;     //M1 Speed Control
+int speedPin_M2 = 6;     //M2 Speed Control
+int directionPin_M1 = 4;     //M1 Direction Control
+int directionPin_M2 = 7;     //M1 Direction Control
+
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
 }
-  
-void loop(){
- 
-      actualDistance = MeasureDistance();
-     Serial.println(actualDistance);
-   delay(500);
- 
+
+void loop() {
+
+  int distance = ultraSound();
+  if (distance < 10) {  // This is where the LED On/Off happens
+    Serial.print("I'm going!");
+    digitalWrite (speedPin_M2, HIGH);             //PWM Speed Control
+    digitalWrite(directionPin_M1, HIGH);
+    digitalWrite (speedPin_M1, HIGH);
+    digitalWrite(directionPin_M2, HIGH);
+
+    //sweepServo();
+  }
+  else {
+    Serial.print("measured: ");
+    Serial.println(distance);
+
+    digitalWrite (speedPin_M2, LOW);             //PWM Speed Control
+    digitalWrite (speedPin_M1, LOW);
+  }
+  if (distance >= 200 || distance <= 0) {
+    Serial.println("Out of range");
+
+  }
+
+  delay(500);
 }
- 
-void SensorSetup(){ 
-  pinMode(URTRIG,OUTPUT);                     // A low pull on pin COMP/TRIG
-  digitalWrite(URTRIG,HIGH);                  // Set to HIGH 
-  pinMode(URPWM, INPUT);                      // Sending Enable PWM mode command
-  for(int i=0;i<4;i++){
-      Serial.write(EnPwmCmd[i]);
-   } 
+
+void sweepServo( void ) {
+  myservo1.attach(10);  // attaches the servo on pin 10 to the servo object
+  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  myservo1.detach(); //note that every time I exit the function I also detach the servo
+  //so there are no intereferences with the ultrasound sensor
 }
- 
-int MeasureDistance(){        // a low pull on pin COMP/TRIG  triggering a sensor reading
-    digitalWrite(URTRIG, LOW);
-    digitalWrite(URTRIG, HIGH);               // reading Pin PWM will output pulses    
-    unsigned long distance=pulseIn(URPWM,LOW);    
-    if(distance==50000){              // the reading is invalid.
-      Serial.print("Invalid");    
-    }else{
-      distance=distance/50;           // every 50us low level stands for 1cm
-    }
-    return distance;
+
+void neck( int pos ) {
+  myservo2.attach(9);  // attaches the servo on pin 10 to the servo object
+  myservo2.write(40 * pos);            // tell servo to go to position in variable 'pos'
+  delay(1000);                       // waits 15ms for the servo to reach the position
+  myservo2.detach(); //note that every time I exit the function I also detach the servo
+  //so there are no intereferences with the ultrasound sensor
 }
- 
-void servoSweep(){
-  if(sweepFlag ){  
-     if(pos>=60 && pos<=120){                   
-        pos=pos+1;                                  // in steps of 1 degree 
-        myservo.write(pos);                         // tell servo to go to position in variable 'pos' 
-    }
-      if(pos>119)  sweepFlag = false;                       // assign the variable again
-  }else {                                       
-      if(pos>=60 && pos<=120){    
-        pos=pos-1;
-        myservo.write(pos);
-      }
-      if(pos<61)  sweepFlag = true;
-   }
+
+
+int ultraSound( void ) {
+  long duration;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  long distance = (duration / 2) / 29.1;
+  return (int)distance;
 }
